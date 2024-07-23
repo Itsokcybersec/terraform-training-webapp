8e64ca4f5cda564766c4c54f16e252ab70205f44
Laeli <laelijr@gmail.com>
2024-07-23 13:59:28 -0700

    adding appsvcplan module 4

--- modules/appsvcplan/main.tf
+++ modules/appsvcplan/main.tf
@@ -6,11 +6,6 @@ resource "azurerm_service_plan" "webapp_service_plan" {
   name                = var.app_service_plan_name
   resource_group_name = data.azurerm_resource_group.rg.name
   location            = data.azurerm_resource_group.rg.location
-#  os_type             = "Windows"
-#  sku_name            = "P1v2"
-
-  sku {
-      os_type = var.app_service_plan.os_type
-      sku_name = var.app_service_plan.sku_name     
-  }
+  os_type             = var.os_type
+  sku_name            = var.sku_name
 }
