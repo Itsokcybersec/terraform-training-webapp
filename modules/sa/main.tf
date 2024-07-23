9fcc951431afa34feeddb26c2e163328a81fdfba
Laeli <laelijr@gmail.com>
2024-07-23 15:10:38 -0700

    adding appsvcplan module 9

--- modules/sa/main.tf
+++ modules/sa/main.tf
@@ -1,7 +1,3 @@
-data "azurerm_resource_group" "cig_tf_poc_rg" {
-  name = var.resource_group_name
-}
-
 resource "azurerm_storage_account" "sa" {
   name = var.saname
   resource_group_name = var.resource_group_name
