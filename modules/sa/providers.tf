cb3ca214253236f40ddb62c28908988dec045291
Laeli <laelijr@gmail.com>
2024-07-22 18:45:07 -0700

    fixing error 24

--- modules/sa/providers.tf
+++ modules/sa/providers.tf
@@ -17,11 +17,3 @@ terraform {
     }
   }
 }
-# Connection for Terraform Cloud - Azure
-provider "azurerm" {
-  client_id = var.azure_client_id
-  client_secret = var.azure_client_secret
-  subscription_id = var.azure_subscription_id
-  tenant_id = var.azure_tenant_id
-  features {}
-}
